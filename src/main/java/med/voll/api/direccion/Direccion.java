13043569068e5e019b0987ac8f8b54ae2ebf2677
Edwin Sánchez <edwin.196@outlook.com>
2025-07-26 14:55:25 -0500

    Se agregan las anotaciones usando lombok, para manejar los constructores y otros complementos

--- src/main/java/med/voll/api/direccion/Direccion.java
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -1,14 +1,25 @@
 package med.voll.api.direccion;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
 public class Direccion {
-    //private Long id;
+
+   private Long id;
    private String calle;
    private String numero;
    private String complemento;
    private String barrio;
    private String codigo_postal;
    private String estado;
-
-
 
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,10 +1,28 @@
 package med.voll.api.medico;
 
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.api.direccion.Direccion;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "Medico")
+@Table(name = "medicos")
 public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String email;
     private String documento;
+
+    @Enumerated(EnumType.STRING)
     private Especialidad especialidad;
+
+    @Embedded
     private Direccion direccion;
 }
