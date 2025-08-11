b3b54104662e660dffc30a7ffe4174446f75ded3
Edwin Sánchez <edwin.196@outlook.com>
2025-08-11 14:05:16 -0500

    se agrega el manejo del error 400 para manejo de validación

--- src/main/java/med/voll/api/infra/GestorDeErrores.java
+++ src/main/java/med/voll/api/infra/GestorDeErrores.java
@@ -1,5 +1,40 @@
 package med.voll.api.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GestorDeErrores {
+
+//Clase para manejar errores
+    //Anotación para errores
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity gestionarError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    //Método para gestionar error de parametros faltantes
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity gestionarError400(MethodArgumentNotValidException ex){
+        var errores = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValidacion::new).toList());
+    }
+    //Se crea un rcord para manejar un dto con los errores
+    //debido a que el dto no se utilizara para otra cosa
+    public record DatosErrorValidacion(String campo, String mensaje){
+
+        public DatosErrorValidacion(FieldError error){
+            this(
+                    error.getField(),
+                    error.getDefaultMessage()
+            );
+        }
+    }
+
+
+
 }
